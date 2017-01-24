58e4de48be18d283ad89f2e9ca6f6101557b93b6
antoinfive <antoinfive@gmail.com>
2017-01-23 19:38:49 -0500

    can create album from search response

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,12 @@
 class Album < ApplicationRecord
-  belongs_to :artist
+  belongs_to :artist, optional: true
+
+  def self.build_from_spotify(data)
+    Album.find_or_create_by(spotify_id: data['id']) do |album|
+      album.spotify_url = data['external_urls']['spotify']
+      album.name = data['name'] 
+      album.release_date = data['release_date']
+      album.image_url = data['images'].first['url'] 
+    end
+  end
 end
