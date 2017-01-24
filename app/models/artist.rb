d5b30edf9b8efff6e048447a42433237ff81cb19
antoinfive <antoinfive@gmail.com>
2017-01-23 19:56:55 -0500

    create track and artist from spotify

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,13 @@
 class Artist < ApplicationRecord
+  has_many :tracks 
+  has_many :albums 
+
+  def self.build_from_spotify(data) 
+    Artist.find_or_create_by(spotify_id: data['id']) do |artist|
+      binding.pry
+      artist.name = data['name']
+      artist.followers = data['followers']['total'] 
+      artist.spotify_url = data['href'] 
+    end 
+  end
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,4 +1,14 @@
 class Track < ApplicationRecord
-  belongs_to :album
-  belongs_to :artist
+  belongs_to :album, optional: true
+  belongs_to :artist, optional: true
+
+    
+  def self.build_from_spotify(data) 
+    Track.find_or_create_by(spotify_id: data['id']) do |track| 
+      track.name = data['name'] 
+      track.spotify_url = data['href'] 
+      track.duration = data['duration_ms'] 
+      track.disc_number = data['disc_number'] 
+    end 
+  end
 end
